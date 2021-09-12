29f7a22bf2ca82dbd951b1cc3f504233f2f714a6
knsgnwn <90548495+knsgnwn@users.noreply.github.com>
2021-09-13 00:24:21 +0700

    Add files via upload

---
+++ KGMusic/services/queues/__init__.py
@@ -0,0 +1,7 @@
+from KGMusic.services.queues.queues import clear 
+from KGMusic.services.queues.queues import get
+from KGMusic.services.queues.queues import is_empty
+from KGMusic.services.queues.queues import put
+from KGMusic.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
